b5968ee665304171be2061a2bc3a5f6cc7f69097
Randhir Singh <randhirsi@vmware.com>
2024-12-03 17:08:52 +0530

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Randhir Singh on 03/12/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
